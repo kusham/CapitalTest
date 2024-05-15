4c1250b1801504a0754e8e041f5947467b88871a
kusham <kusham1998@gmail.com>
2024-05-15 20:17:51 +0530

    create DTOs

--- Models/Programs.cs
+++ Models/Programs.cs
@@ -7,9 +7,7 @@ namespace CapitalTest.Models
     {
         [JsonProperty("id")]
         public required Guid Id { get; set; }
-        [Required(ErrorMessage = "Program title is required")]
         public required string ProgramTitle { get; set; }
-        [Required(ErrorMessage = "Program description is required")]
         public required string ProgramDescription { get; set; }
         public ICollection<Questions> Questions { get; set; } = [];
     }
