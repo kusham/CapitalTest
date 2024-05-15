57381e923f9a6b5398e40dacd6b50232b1208652
kusham <kusham1998@gmail.com>
2024-05-15 19:59:34 +0530

    create models

---
+++ Models/Programs.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace CapitalTest.Models
+{
+    public class Programs
+    {
+        [JsonProperty("id")]
+        public required Guid Id { get; set; }
+        [Required(ErrorMessage = "Program title is required")]
+        public required string ProgramTitle { get; set; }
+        [Required(ErrorMessage = "Program description is required")]
+        public required string ProgramDescription { get; set; }
+        public ICollection<Questions> Questions { get; set; } = [];
+    }
+}
